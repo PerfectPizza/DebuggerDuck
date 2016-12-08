dafb767aa343577a922444218bef128d8a82d0ce
Ross Topol <rtopol2@gmail.com>
2016-12-08 12:02:30 -0600

    Update DB info

--- server/db/connection.js
+++ server/db/connection.js
@@ -3,7 +3,7 @@ const mongoose = require ('mongoose');
 //This must be called because mongoose's promises library is deprecated
 mongoose.Promise = global.Promise;
  
-const mongodbUri = 'mongodb://Robin:robin1@ds119588.mlab.com:19588/debuggerduck';
+const mongodbUri = 'mongodb://perfectpizza:perfectPizza8@ds129038.mlab.com:29038/heroku_jtrfbw7c';
 mongoose.connect(mongodbUri);
 
 const conn = mongoose.connection;   
@@ -12,4 +12,4 @@ conn.on('error', console.error.bind(console, 'MongoDB connection error:'));
 module.exports = conn;
 
 //FYI, if you want play with the data in the command line, use this:
-// mongo ds119588.mlab.com:19588/debuggerduck -u Robin -p robin1
+// mongo ds129038.mlab.com:29038/heroku_jtrfbw7c -u perfectpizza -p perfectPizza8
