71cf8ac45429a841e80f2dad787ea37e96f1db65
Ross Topol <rtopol2@gmail.com>
2016-12-08 17:04:41 -0600

    Add DB connection console logs

--- server/db/connection.js
+++ server/db/connection.js
@@ -7,8 +7,23 @@ mongoose.Promise = global.Promise;
 const mongodbUri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ds129038.mlab.com:29038/heroku_jtrfbw7c`;
 mongoose.connect(mongodbUri);
 
-const conn = mongoose.connection;   
-conn.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+const conn = mongoose.connection
+// CONNECTION EVENTS
+// When successfully connected
+conn.on('connected', function () {  
+  console.log('Mongoose connection open');
+}); 
+
+// If the connection throws an error
+conn.on('error',function (err) {  
+  console.log('Mongoose default connection error: ' + err);
+}); 
+
+// When the connection is disconnected
+conn.on('disconnected', function () {  
+  console.log('Mongoose default connection disconnected'); 
+});
  
 module.exports = conn;
 
