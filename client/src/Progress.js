5628dfa36f0852b2ae096d1d94ec1e88f02bae76
Kelly Braun <kellyjohnbraun@gmail.com>
2016-12-10 17:16:40 -0600

    making PROGRESS, hopefully

--- client/src/Progress.js
+++ client/src/Progress.js
@@ -5,6 +5,19 @@ class Progress extends Component {
     super(props);
     }
   
+  componentDidMount(){
+    socket.on('status' + this.props.orderId, function(status){
+      if (status === 'Fetcher Departed'){
+        $('#task1').toggleClass( "todo done");
+      }
+      if (status === 'Fetcher Returning'){
+        $('#task2').toggleClass( "todo done");
+      }
+      if (status === 'Food Arrived'){
+        $('#task3').toggleClass( "todo done");
+      }
+    })
+  }
   //save to state
   render(){
     //change class between todo and done
@@ -12,7 +25,7 @@ class Progress extends Component {
       <div className="progress-div">
     		<div className="progress-container">
           <ol className="progress-meter">
-            <li className="progress-point done">05:20 mins</li><li className="progress-point done">05:10 mins</li><li className="progress-point done">Link</li><li className="progress-point todo">Connect</li>
+            <li id='task1' className="progress-point todo">Fetching Food</li><li id='task2' className="progress-point todo">Food Inbound</li><li id='task3' className="progress-point todo">Food is Here</li>
           </ol>
         </div>
       </div>
