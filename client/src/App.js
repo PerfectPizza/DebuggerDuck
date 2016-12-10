5d0ec3463ad9965ccdfb2cc62d85ee7f38384802
MKS-students <mks.students@hackreactor.com>
2016-12-10 12:37:45 -0600

    rebasing

--- client/src/App.js
+++ client/src/App.js
@@ -287,7 +287,7 @@ class Runner extends Component {
         console.log('ROLE IS :', this.state.role)
         return(
           <div>
-            <StatusView orderId={this.state.currentData} username={this.state.username} picture={this.state.picture} role={this.state.role} changeRole={this.changeRole.bind(this)}/>
+            <StatusView orderId={this.state.currentData || null} username={this.state.username} picture={this.state.picture} role={this.state.role} changeRole={this.changeRole.bind(this)}/>
           </div>
           )
       }
